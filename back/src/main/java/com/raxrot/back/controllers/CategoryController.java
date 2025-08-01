fcd0a964c499a485185480c3bcf8e0126b32c191
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-01 16:32:31 +0100

    add CategoryController with create,readall,delete

---
+++ back/src/main/java/com/raxrot/back/controllers/CategoryController.java
@@ -0,0 +1,37 @@
+package com.raxrot.back.controllers;
+
+import com.raxrot.back.dtos.CategoryRequest;
+import com.raxrot.back.dtos.CategoryResponse;
+import com.raxrot.back.services.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+private final CategoryService categoryService;
+public CategoryController(CategoryService categoryService) {
+    this.categoryService = categoryService;
+}
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CategoryRequest request) {
+        CategoryResponse response=categoryService.create(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+    List<CategoryResponse> response=categoryService.getAllCategories();
+    return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<Void> deleteCategoryById(@PathVariable String categoryId) {
+        categoryService.deleteCategoryById(categoryId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
