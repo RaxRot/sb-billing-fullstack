6a032f60dab7089e4a5f98e599407ae14a2f616d
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-01 16:31:53 +0100

    add constracrurs

--- back/src/main/java/com/raxrot/back/dtos/CategoryRequest.java
+++ back/src/main/java/com/raxrot/back/dtos/CategoryRequest.java
@@ -1,11 +1,15 @@
 package com.raxrot.back.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryRequest {
     @NotBlank(message = "Name is required")
     private String name;
--- back/src/main/java/com/raxrot/back/dtos/CategoryResponse.java
+++ back/src/main/java/com/raxrot/back/dtos/CategoryResponse.java
@@ -1,12 +1,16 @@
 package com.raxrot.back.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryResponse {
     private String categoryId;
     private String name;
