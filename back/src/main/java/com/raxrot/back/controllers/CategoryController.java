69e52c7c4d7133ae5d4671cce41c7a20fdbe4a01
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-01 21:16:14 +0100

    fix code to work with files

--- back/src/main/java/com/raxrot/back/controllers/CategoryController.java
+++ back/src/main/java/com/raxrot/back/controllers/CategoryController.java
@@ -1,12 +1,17 @@
 package com.raxrot.back.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.raxrot.back.dtos.CategoryRequest;
 import com.raxrot.back.dtos.CategoryResponse;
 import com.raxrot.back.services.CategoryService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,15 +19,27 @@ import java.util.List;
 @RequestMapping("/categories")
 public class CategoryController {
     private final CategoryService categoryService;
+    private final ObjectMapper objectMapper;
 
-    public CategoryController(CategoryService categoryService) {
+    public CategoryController(CategoryService categoryService, ObjectMapper objectMapper) {
         this.categoryService = categoryService;
+        this.objectMapper = objectMapper;
     }
 
-    @PostMapping
-    public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CategoryRequest request) {
-        CategoryResponse response = categoryService.create(request);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CategoryResponse> create(
+            @RequestPart("category")String categoryString,
+            @RequestPart("file") MultipartFile file) {
+
+        try {
+            CategoryRequest request=objectMapper.readValue(categoryString,CategoryRequest.class);
+            CategoryResponse response=categoryService.create(request,file);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        }  catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid category JSON", e);
+        }
     }
 
     @GetMapping
