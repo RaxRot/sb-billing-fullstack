d68bce4bc1e019775db1176200cdd399363942b3
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-01 21:16:05 +0100

    fix code to work with files

--- back/src/main/java/com/raxrot/back/services/CategoryService.java
+++ back/src/main/java/com/raxrot/back/services/CategoryService.java
@@ -2,11 +2,12 @@ package com.raxrot.back.services;
 
 import com.raxrot.back.dtos.CategoryRequest;
 import com.raxrot.back.dtos.CategoryResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface CategoryService {
-    CategoryResponse create(CategoryRequest request);
+    CategoryResponse create(CategoryRequest request, MultipartFile file);
     List<CategoryResponse> getAllCategories();
     void deleteCategoryById(String categoryId);
 }
--- back/src/main/java/com/raxrot/back/services/impl/CategoryServiceImpl.java
+++ back/src/main/java/com/raxrot/back/services/impl/CategoryServiceImpl.java
@@ -6,8 +6,10 @@ import com.raxrot.back.exceptions.ApiException;
 import com.raxrot.back.models.Category;
 import com.raxrot.back.repositories.CategoryRepository;
 import com.raxrot.back.services.CategoryService;
+import com.raxrot.back.services.FileUploadService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,19 +19,26 @@ import java.util.stream.Collectors;
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
     private final ModelMapper modelMapper;
-    public CategoryServiceImpl(CategoryRepository categoryRepository, ModelMapper modelMapper) {
+    private final FileUploadService fileUploadService;
+    public CategoryServiceImpl(CategoryRepository categoryRepository, ModelMapper modelMapper, FileUploadService fileUploadService) {
         this.categoryRepository = categoryRepository;
         this.modelMapper = modelMapper;
+        this.fileUploadService = fileUploadService;
     }
 
     @Override
-    public CategoryResponse create(CategoryRequest request) {
+    public CategoryResponse create(CategoryRequest request, MultipartFile file) {
         if (categoryRepository.existsByName(request.getName())) {
             throw new ApiException("Category with name " + request.getName() + " already exists");
         }
+
         Category category = modelMapper.map(request, Category.class);
+
+        String imageUrl=fileUploadService.uploadFile(file);
         String categoryId= UUID.randomUUID().toString();
         category.setCategoryId(categoryId);
+        category.setImageUrl(imageUrl);
+
         Category savedCategory = categoryRepository.save(category);
         return modelMapper.map(savedCategory, CategoryResponse.class);
     }
@@ -47,6 +56,7 @@ public class CategoryServiceImpl implements CategoryService {
     public void deleteCategoryById(String categoryId) {
         Category category=categoryRepository.findByCategoryId(categoryId)
                 .orElseThrow(()-> new ApiException("Category with id " + categoryId + " does not exist"));
+        fileUploadService.deleteFile(category.getImageUrl());
         categoryRepository.delete(category);
     }
 }
